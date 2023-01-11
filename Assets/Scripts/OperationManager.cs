e9af2dc904bf78c67f83fad6f19458d1e579205d
Shem Tom <63319003+shemtomke@users.noreply.github.com>
2023-01-11 20:30:04 +0300

    update

--- Assets/Scripts/OperationManager.cs
+++ Assets/Scripts/OperationManager.cs
@@ -9,61 +9,28 @@ public class OperationManager : MonoBehaviour
     //bottom text used to input and add
     public Text bottomText, topText;
 
-    private double currentValue = 0;
-    private string currentOperator;
-
     //display value in the bottom value
     public void DisplayValue(string number)
     {
         bottomText.text += number;
-        currentValue += double.Parse(number);
     }
     public void DisplayOperator(string op)
     {
-        currentOperator = op;
         bottomText.text += op;
     }
 
     public void Clear()
     {
-        currentValue = 0;
         bottomText.text = "";
         topText.text = "";
     }
-
     public void Calculate()
     {
-        //value in the bottom text goes to the top text and bottom text updates the answer
-        topText.text = bottomText.text;
+        string value = bottomText.text.ToString();
 
-        //if the operation ends or starts with an operator then you cannot perform a calculation
-        /*if ((bottomText.text.Substring(bottomText.text.Length - 1) == operatorValue) || (bottomText.text.Substring(0) == operatorValue))
-        {
-            bottomText.text = "ERROR!";
-        }*/
-        //perform calucaltion by dividing the operators within the current value
-        //bottomText.text = "";
-        switch (currentOperator)
-        {
-            case "+":
-                currentValue += double.Parse(bottomText.text);
-                bottomText.text = currentValue.ToString();
-                break;
-            case "-":
-                currentValue -= double.Parse(bottomText.text);
-                bottomText.text = currentValue.ToString();
-                break;
-            case "*":
-                currentValue *= double.Parse(bottomText.text);
-                bottomText.text = currentValue.ToString();
-                break;
-            case "/":
-                currentValue /= double.Parse(bottomText.text);
-                bottomText.text = currentValue.ToString();
-                break;
-        }
+        topText.text = bottomText.text; //operation done to be displayed
 
-        currentOperator = null;
+        //display answer to the bottom text
+
     }
-
 }
